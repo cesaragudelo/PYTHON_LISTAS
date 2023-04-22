36b7493019177d08a5938f002954e1376403ed89
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-04-21 20:49:56 -0500

    listas python

---
+++ listas.py
@@ -0,0 +1,17 @@
+# trabajando estucturas de  datos en listas
+Estudiantes= ['julian','moises','santiago','Cesar']
+
+print ('El primer estudiante de la lista es: '+Estudiantes[0])
+print('todos los estudiantes de la lista son: ',Estudiantes)
+#para saber el tamaño de la lista
+print ('la cantidad de estudiantes es: ',len(Estudiantes))
+#imprimir el ultimop estudiante de la lista
+print ('el ultimo estudiantes de la lista es: ',Estudiantes[len(Estudiantes)-1])
+#agregar un elemento al final de la lista
+Estudiantes.append('laura')
+print ('el ultimo estudiantes de la lista es: ',Estudiantes[len(Estudiantes)-1])
+#ordena la lista
+Estudiantes.sort()
+print('todos los estudiantes de la lista son: ',Estudiantes)
+
+
